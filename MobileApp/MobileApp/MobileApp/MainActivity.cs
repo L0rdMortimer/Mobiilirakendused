6acb501f48027c96b47c9a87e314b2425220e712
L0rdMortimer <33531910+L0rdMortimer@users.noreply.github.com>
2021-01-30 15:04:46 +0200

    Tunnitoo valmis

--- MobileApp/MobileApp/MobileApp/MainActivity.cs
+++ MobileApp/MobileApp/MobileApp/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace MobileApp
 {
@@ -15,6 +16,29 @@ namespace MobileApp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var counterView = FindViewById<TextView>(Resource.Id.textView2);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            int counter = 0;
+            var toCalculatorButton = FindViewById<Button>(Resource.Id.toCalculator);
+
+            button.Click += delegate
+            {
+                textView.Text = "Hello Worms";
+                counter += 1;
+                counterView.Text = counter.ToString();
+                
+            };
+
+            toCalculatorButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(CalculatorActivity));
+                StartActivity(intent);
+            };
+
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
